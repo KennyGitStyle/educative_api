26eea9b54bc349c2444e81a47a1cd62400bf3a9f
Kenneth Hendricks <kennethhendricks10@gmail.com>
2022-07-31 12:11:29 +0100

    Eager Loading

--- Educative.Infrastructure/Repository/StudentRepository.cs
+++ Educative.Infrastructure/Repository/StudentRepository.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Educative.Core;
 using Educative.Infrastructure.Context;
 using Educative.Infrastructure.Interface;
+using Microsoft.EntityFrameworkCore;
 
 namespace Educative.Infrastructure.Repository
 {
@@ -12,13 +14,21 @@ namespace Educative.Infrastructure.Repository
     StudentRepository
     : GenericRepository<Student>, IStudentRepository
     {
+        private readonly EducativeContext _context;
+
         public StudentRepository(EducativeContext context) : base(context)
         {
+            _context = context;
         }
 
-        public Task<IEnumerable<Student>> FilterAttendanceAsync(Expression<Func<Student, bool>> predicate)
+        public async Task<IEnumerable<Student>> FilterAttendanceAsync(Expression<Func<Student, bool>> predicate)
         {
-            throw new NotImplementedException();
+             return await _context.Students
+                .Include(s => s.Attendance >= 80)
+                .Include(sa => sa.Address.City == "New York")
+                .Include(sc => sc.StudentCourses)
+                .ThenInclude(cs => cs.CourseId == "Bus")
+                .Where(predicate).OrderBy(x => x.Lastname).ToListAsync();   
         }
     }
 }
